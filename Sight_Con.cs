be0ba309af5643f70444721b68f6cb24dbc7299b
CLe4N <64629943+CLe4N@users.noreply.github.com>
2020-11-22 05:31:29 +0700

    Update Sight_Con.cs

--- Sight_Con.cs
+++ Sight_Con.cs
@@ -7,9 +7,9 @@ public class Sight_Con : MonoBehaviour
 
     void Update()
     {
-        Vector3 mousePos = Input.mousePosition;
-        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector3 mousePos = Input.mousePosition; // get mouse position
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos); // calculate mosue position from screen position to world position
 
-        transform.position = new Vector2(mousePos.x, mousePos.y);
+        transform.position = new Vector2(mousePos.x, mousePos.y); // change this gameobject position to mouse position
     }
 }
