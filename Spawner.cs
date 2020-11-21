dc2a1bf435962f01182d0c6c674d98d31c463578
CLe4N <64629943+CLe4N@users.noreply.github.com>
2020-11-22 05:38:29 +0700

    Update Spawner.cs

--- Spawner.cs
+++ Spawner.cs
@@ -15,52 +15,53 @@ public class Spawner : MonoBehaviour
 
     private void Start()
     {
-        zombieRoar = GameObject.Find("ZombieRoar").transform.GetComponent<AudioSource>();
+        zombieRoar = GameObject.Find("ZombieRoar").transform.GetComponent<AudioSource>(); // find "ZombieRoar" and access AudioSource Component
     }
     void Update()
     {
-        if (SpawnTime > 0.99)
+        if (SpawnTime > 0.99) // if spawntime more than 0.99
         {
-            if (Is_CD == false)
+            if (Is_CD == false) // if Is_CD = false
             {
-                spawnCD = 10;
-                Is_CD = true;
+                spawnCD = 10; // spawnCD = 10
+                Is_CD = true; // Is_CD = true
             }
             else
             {
-                if (spawnCD > 0)
+                if (spawnCD > 0) // if spawnCD more than 0
                 {
-                    spawnCD -= Time.deltaTime;
+                    spawnCD -= Time.deltaTime; // spawnCD decrease by 1 per second
                 }
                 else
                 {
-                    Is_CD = false;
-                    spawnCD = 0;
-                    SpawnTime -= 0.2f;
+                    Is_CD = false; // Is_CD = false
+                    spawnCD = 0; // spawnCD = 0
+                    SpawnTime -= 0.2f; // SpawnTime decrease by 0.2
                 }
             }
         }
-        Respawn(SpawnTime);
+        Respawn(SpawnTime); // call method Respawn
     }
 
     void Respawn(float RespawmCD)
     {
-        if (IsRespawn == false)
+        if (IsRespawn == false) // if IsRespawn == false
         {
-            Instantiate(ZombiePrefab, new Vector2(Random.Range(-6, 6), transform.position.y), Quaternion.identity);
-            zombieRoar.Play();
-            IsRespawn = true;
-            coolDown = RespawmCD;
+            Instantiate(ZombiePrefab, new Vector2(Random.Range(-6, 6), transform.position.y), Quaternion.identity); 
+            // create prefab ZombiePrefab x position random between -6 to 6 
+            zombieRoar.Play(); // play zombieRoar
+            IsRespawn = true; // IsRespawn = true
+            coolDown = RespawmCD; // coolDown = RespawmCD
         }
-        if(IsRespawn == true)
+        if(IsRespawn == true) // if IsRespawn == true
         {
-            if(coolDown > 0)
+            if(coolDown > 0) // if coolDown > 0
             {
-                coolDown -= Time.deltaTime;
+                coolDown -= Time.deltaTime; // coolDown decrease by 1 per sec
             }
             else
             {
-                IsRespawn = false;
+                IsRespawn = false; // IsRespawn = false;
             }
         }
     }
